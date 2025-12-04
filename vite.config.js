e66b27d218223f459b0e3f54ddf70842c18fc723
Donadavid018 <donadavid018@gmail.com>
2025-12-04 13:50:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 const repoName = '/Arcnetic/';
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? repoName : '/',
+  base: '/',
   plugins: [react()],
 })
